42d813f07d844d2ef9f1f966923e515367fe5ed6
jhon <jhon@ubuntuServer.home>
2018-12-11 06:51:52 +0100

    Begin User Doc

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,7 +7,9 @@ use App\Service\UserService;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Request\ParamFetcherInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -16,6 +18,19 @@ use Symfony\Component\Validator\ConstraintViolationListInterface;
 class UserController extends FOSRestController
 {
     /**
+     * @SWG\Get(
+     *     tags={"User"},
+     *     summary="Get The Detail of User",
+     *     description="Return one user",
+     *     @SWG\Response(
+     *          response=200,
+     *          description="Success",
+     *          @Model(
+     *              type=User::class
+     *          )
+     *     )
+     * )
+     *
      * @Rest\Get(
      *     path = "/api/users/{id}",
      *     name = "app_user_show",
