9baca4b277cb70ab7fcfab4ad3493963a7a39f45
jhon <jhon@ubuntuServer.home>
2018-12-11 06:40:43 +0100

    refacto Products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,7 +6,6 @@ use App\Service\ProductService;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
-use Nelmio\ApiDocBundle\Annotation\Security;
 use Swagger\Annotations as SWG;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Product;
@@ -59,7 +58,7 @@ class ProductController extends AbstractController
     /**
      * @SWG\Get(
      *     tags={"Product"},
-     *     summary="Get the list of product",
+     *     summary="Get the list of products",
      *     description="Return a paginated collection of products",
      *     consumes={"application/json"},
      *     produces={"application/json"},
